1398b81d988ad91f8e58516e5a74e36cd2560d5f
caiop <caiop@LAPTOP-OP707Q9G>
2018-08-21 22:15:46 -0300

    tabela pronta

--- src/java/NameBean.java
+++ src/java/NameBean.java
@@ -10,20 +10,72 @@ import javax.inject.Named;
 @SessionScoped
 public class NameBean implements Serializable {
 
-       private static final ArrayList<Name> nameList
-      = new ArrayList<Name>(Arrays.asList(
-      new Name("John", "Marketing"),
-      new Name("Robert", "Marketing"),
-      new Name("Mark", "Sales") )      
-    );
+    private static ArrayList<Name> nameList;
 
     public NameBean() {
+        nameList = new ArrayList<Name>();
+        nameList.add(new Name("John", "Marketing"));
+        nameList.add(new Name("John", "1"));
+        nameList.add(new Name("John", "2"));
+        nameList.add(new Name("John", "Marketing"));
+        nameList.add(new Name("John", "1"));
+        nameList.add(new Name("John", "2"));
+        nameList.add(new Name("John", "Marketing"));
+        nameList.add(new Name("John", "1"));
+        nameList.add(new Name("John", "2"));
+        nameList.add(new Name("John", "Marketing"));
+        nameList.add(new Name("John", "1"));
+        nameList.add(new Name("John", "2"));
+        nameList.add(new Name("John", "Marketing"));
+        nameList.add(new Name("John", "1"));
+        nameList.add(new Name("John", "2"));
+        nameList.add(new Name("John", "Marketing"));
+        nameList.add(new Name("John", "1"));
+        nameList.add(new Name("John", "2"));
+        nameList.add(new Name("John", "Marketing"));
+        nameList.add(new Name("John", "1"));
+        nameList.add(new Name("John", "2"));
+        nameList.add(new Name("John", "Marketing"));
+        nameList.add(new Name("John", "1"));
+        nameList.add(new Name("John", "2"));
+        nameList.add(new Name("John", "Marketing"));
+        nameList.add(new Name("John", "1"));
+        nameList.add(new Name("John", "2"));
+        nameList.add(new Name("John", "Marketing"));
+        nameList.add(new Name("John", "1"));
+        nameList.add(new Name("John", "2"));
+        nameList.add(new Name("John", "Marketing"));
+        nameList.add(new Name("John", "1"));
+        nameList.add(new Name("John", "2"));
+        nameList.add(new Name("John", "Marketing"));
+        nameList.add(new Name("John", "1"));
+        nameList.add(new Name("John", "2"));
+
     }
 
-       
     public ArrayList<Name> getNameList() {
         return nameList;
     }
 
+    public String saveTodosNames() {
+
+        //get all existing value but set "editable" to false 
+        for (Name nameList : nameList) {
+            nameList.setIsEditable(false);
+        }
+        //return to current page
+        return null;
+
+    }
+
+    public String deleteName(Name n) {
+        nameList.remove(n);
+        return null;
+    }
+
+    public String salvaName(Name n) {
+        n.setIsEditable(false);
+        return null;
+    }
 
 }
